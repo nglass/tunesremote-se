f16b9531a04965ddcc77de96bb8b3718624fa14d
Melloware <Melloware@ea70d31f-9cf3-621d-82d4-ea082a12fb48>
2011-09-21 16:53:00 +0000

    Fixes Issues 15, 16, 19 and temporarily disables issue 17 but now it is causing other UI issues.

--- branches/melloware-1/TunesRemoteSE/src/org/libtunesremote_se/LibraryDetails.java
+++ branches/melloware-1/TunesRemoteSE/src/org/libtunesremote_se/LibraryDetails.java
@@ -5,61 +5,64 @@ package org.libtunesremote_se;
  */
 
 public class LibraryDetails {
-	private String libraryName;
-	private String serviceName;
-	private String address;
-	private String library;
-	private int port;
+   private final String libraryName;
+   private final String libraryType;
+   private final String serviceName;
+   private String address;
+   private final String library;
+   private final int port;
 
-	public LibraryDetails
-		(String libraryName, 
-		 String serviceName, 
-		 String address, 
-		 String library, 
-		 int port) {
-		
-		this.libraryName = libraryName;
-		this.serviceName = serviceName;
-		this.address = address;
-		this.library = library;   
-		this.port = port;
-	}
-	
-	public String getLibraryName() {
-		return libraryName;
-	}
-	
-	public String getServiceName() {
-		return serviceName;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-	
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	public String getLibrary() {
-		return library;
-	}
+   public LibraryDetails(String libraryName, String libraryType, String serviceName, String address, String library, int port) {
 
-	public int getPort() {
-		return port;
-	}
-	
-	// this is how the object will appear in the list
-	@Override
-	public String toString() {
-		return libraryName;	
-	}
-	
-	@Override
-	public boolean equals(Object aThat){
-		if ( this == aThat ) return true;
-		if ( !(aThat instanceof LibraryDetails) ) return false;
-		LibraryDetails that = (LibraryDetails)aThat;
-		return this.serviceName.equals(that.serviceName);
-	}
+      this.libraryName = libraryName;
+      this.libraryType = libraryType;
+      this.serviceName = serviceName;
+      this.address = address;
+      this.library = library;
+      this.port = port;
+   }
+
+   public String getLibraryName() {
+      return libraryName;
+   }
+
+   public String getServiceName() {
+      return serviceName;
+   }
+
+   public String getAddress() {
+      return address;
+   }
+
+   public void setAddress(String address) {
+      this.address = address;
+   }
+
+   public String getLibrary() {
+      return library;
+   }
+
+   public int getPort() {
+      return port;
+   }
+
+   public String getLibraryType() {
+      return libraryType;
+   }
+
+   // this is how the object will appear in the list
+   @Override
+   public String toString() {
+      return libraryName;
+   }
+
+   @Override
+   public boolean equals(Object aThat) {
+      if (this == aThat)
+         return true;
+      if (!(aThat instanceof LibraryDetails))
+         return false;
+      LibraryDetails that = (LibraryDetails) aThat;
+      return this.serviceName.equals(that.serviceName);
+   }
 }
